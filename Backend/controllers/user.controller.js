334ad3aae0546092cdb942a50744d4e35f3622fa
Avirup Saha <sahaavirup1998@gmail.com>
2025-09-27 20:23:56 +0530

    add and create loginuser api

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,8 +2,8 @@ const userModel = require('../models/user.model.js');
 const userService = require('../services/user.services.js');
 const {validationResult} = require('express-validator')
 
+// create register controller
 module.exports.registerUser = async (req, res, next) => {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -29,6 +29,30 @@ module.exports.registerUser = async (req, res, next) => {
     const token = user.generateAuthToken();
 
     res.status(201).json({ token, user });
+}
 
+// create login user
+module.exports.loginUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({email}).select('password');
+
+    if(!user) {
+        return res.status(401).json({message: "Invalid username or password!"});
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({message: "Invalid username or password!"})
+    }
+
+    const token = user.generateAuthToken();
+
+    res.status(201).json({ token, user });
 }
