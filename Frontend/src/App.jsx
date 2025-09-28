dd3e723b1604f5313da0ad0886a0c60c299ae13b
Avirup Saha <sahaavirup1998@gmail.com>
2025-09-28 20:21:39 +0530

    create the react app for frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-blue-500 text-white'>
+      <h1>Welcome to the Uber Clone</h1>
+    </div>
+  )
+}
+
+export default App
