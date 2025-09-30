68d64d2a47fe04861de2702fd01570f255a90c65
Avirup Saha <sahaavirup1998@gmail.com>
2025-09-30 19:40:12 +0530

    edit the home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen w-full flex justify-between flex-col pt-8'>
             <img className='w-16 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber Logo" />
             <div className='bg-white py-4 px-4 pb-7'>
-                <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
+                <h2 className='text-[30px] font-bold'>Get Started with Uber</h2>
                 <Link className='flex items-center justify-center bg-black text-white py-3 w-full rounded mt-4' to="/login">Continue</Link>
             </div>
         </div>
